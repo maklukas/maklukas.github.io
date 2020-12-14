3dab5d4a6b570ae991119d5aa41cae21b23d5927
maklukas <makuch1994@gmail.com>
2020-12-14 18:40:19 +0100

    change in js in apiRoot on heroku ap

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task/';
+  const apiRoot = 'https://pure-headland-37687.herokuapp.com/v1/trello/';
   const trelloApiRoot = 'https://pure-headland-37687.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
